f4fba9cc5a9e64d5c5fbc049fe97bfca23d12800
stackdev37 <DouglasLuther0411@outlook.com>
2022-06-21 15:20:55 +0900

    configuration pom

---
+++ src/main/java/com/stack37/spring_boot_init/SpringBootInitApplication.java
@@ -0,0 +1,13 @@
+package com.stack37.spring_boot_init;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootInitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootInitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/stack37/spring_boot_init/SpringBootInitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stack37.spring_boot_init;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootInitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
